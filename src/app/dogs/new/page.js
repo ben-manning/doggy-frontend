03795251786f33d3c5125f8e5831d5e5d7565bad
ben-manning <bmanning@me.com>
2023-05-22 11:37:58 -0400

    add POST functionality

---
+++ src/app/dogs/new/page.js
@@ -0,0 +1,47 @@
+'use client';
+
+// import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function NewDogForm() {
+  const [newDog, setNewDog] = useState({name: "", age: 0})
+
+  const router = useRouter()
+
+  const handleChange = (evt) => {
+    setNewDog({ ...newDog, [evt.target.id]: evt.target.value })
+  }
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const res = await fetch('https://doggy-app-backend.herokuapp.com/dogs', {
+      headers: { 'Content-Type' : 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({ name: newDog.name, age: Number(newDog.age) })
+    })
+
+    const dog = await res.json()
+
+    if (dog) {
+      router.push('/dogs')
+    }
+  }
+
+  return (
+
+    <div>
+      <h1>New Dog Form</h1>
+      <form onSubmit={handleSubmit}>
+      <label for="name">Name:
+        <input type="text" name="name" id="name" onChange={handleChange} ></input>
+      </label>
+      <label for="age">Age:
+        <input type="number" name="age" id="age" onChange={handleChange} ></input>
+      </label>
+      
+      <input type="submit" value="Create Dog" />
+      </form>
+    </div>
+  )
+} 
