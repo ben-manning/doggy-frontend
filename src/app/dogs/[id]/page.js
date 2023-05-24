beaa2430a8af4dc8a5da72ac9ec53432aa4f3ff9
ben-manning <bmanning@me.com>
2023-05-24 10:49:04 -0400

    add delete functionality

--- src/app/dogs/[id]/page.js
+++ src/app/dogs/[id]/page.js
@@ -1,11 +1,13 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
 import Link from 'next/link';
 
 export default function Dog({ params }) {
 
   const [dog, setDog] = useState({});
+  const router = useRouter()
 
   useEffect(() => {
 
@@ -19,11 +21,29 @@ export default function Dog({ params }) {
       
   }, [ params.id ]);
 
+  const removeDog = async () => {
+    const res = await fetch(`https://doggy-app-backend.herokuapp.com/dogs/${ params.id }`, {
+      headers: { 'Content-Type' : 'application/json' },
+      method: 'DELETE',
+    })
+
+    const adoptedDog = await res.json()
+
+    if (adoptedDog) {
+      router.push('/dogs')
+    }
+    
+  }
+
   return (
     <div>
       <h2>Here is your dog</h2>
       <h4>Name: { dog.name }</h4>
       <h4>Age: { dog.age }</h4>
+
+      <button onClick={removeDog}>Remove Dog</button>
+
+      <br></br>
 
       <Link href="/dogs">Back to all dogs</Link>
     </div>
